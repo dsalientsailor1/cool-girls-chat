22d747aea69a79ba0c1a5f8f31335b21516819ba
dsalientsailor1 <acnwogbo@gmail.com>
2022-06-07 19:52:07 +0100

    e

--- utils/database.js
+++ utils/database.js
@@ -10,11 +10,6 @@ const mongoConnect = callback => {
 
   // // mongodb+srv://coolgirls:<password>@cluster0.p49sb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
        
-
-  // const uri = "mongodb+srv://<username>:<password>@<your-cluster-url>/test?retryWrites=true&w=majority";
-  // 'mongodb+srv://coolgirl_coolgirls:SvcpilotNnc123*@127.0.0.1/conversations?retryWrites=true&w=majority
-
-
   
 
 
@@ -22,7 +17,6 @@ const mongoConnect = callback => {
 // var MongoClient = require('mongodb').MongoClient;
 // var url = "mongodb://localhost:27017/coolgirl_coolgirls";
 var url = "mongodb+srv://coolgirls:SvcpilotNnc123*@cluster0.vuedel2.mongodb.net/?retryWrites=true&w=majority";
-
 
 
 
